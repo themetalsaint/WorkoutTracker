7040c5370ece948d9aecb8ab7bae8e56cccacbfd
Rachael Davis <80214032+themetalsaint@users.noreply.github.com>
2021-08-10 21:28:15 -0400

    uncommented aggregate

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -2,43 +2,43 @@
 const router = require("express").Router();
 const Workout = require("../models/workout.js");
 
-// router.get("/api/workouts", (req, res) => {
-//     dbWorkout.aggregate([
-//         {
-//             $addFields: {
-//                 totalDuration: {
-//                     $sum:"$exercises.duration"
-//                 }
-//             }
-//         }
-//     ])
-// })
-// .then(dbWorkouts => {
-//     res.json(dbWorkouts)
-// }) 
-// .catch(err => {
-//     res.json(err)
-// })
+router.get("/api/workouts", (req, res) => {
+    dbWorkout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum:"$exercises.duration"
+                }
+            }
+        }
+    ])
+})
+.then(dbWorkouts => {
+    res.json(dbWorkouts)
+}) 
+.catch(err => {
+    res.json(err)
+})
 
-// router.get("/api/workouts/range", (req, res) => {
-//     Workout.aggregate([
-//         {
-//             $addFields: {
-//                 totalDuration: {
-//                     $sum:"$exercises.duration"
-//                 },
-//             },
-//         },
-//     ])
-//     .sort({_id:-1})
-//     .limit(7) //play with this number
-    // .then(dbWorkouts => {
-    //     res.json(dbWorkouts) //console.log(dbWorkouts)
-    // }) 
-//     .catch(err => {
-//         res.json(err)
-//     })
-// })
+router.get("/api/workouts/range", (req, res) => {
+    Workout.aggregate([
+        {
+            $addFields: {
+                totalDuration: {
+                    $sum:"$exercises.duration"
+                },
+            },
+        },
+    ])
+    .sort({_id:-1})
+    .limit(7) //play with this number
+    .then(dbWorkouts => {
+        res.json(dbWorkouts) //console.log(dbWorkouts)
+    }) 
+    .catch(err => {
+        res.json(err)
+    })
+})
 
 router.get("/api/workouts", ({body}, res) => {
     console.log(Workout)
